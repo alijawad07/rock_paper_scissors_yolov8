2cc976bc217a0d930a4947ff768292bee2608443
Ali Jawad <32601057+alijawad07@users.noreply.github.com>
2023-05-28 12:40:03 +0500

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -15,8 +15,6 @@ def parse_args():
     
 args = parse_args()
 
-print(args.source)
-
 # Load the model
 model = YOLO(args.weights)
 
@@ -50,15 +48,7 @@ frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 out_video = cv2.VideoWriter(os.path.join(args.output, "output.mp4"), cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_width, frame_height))
 
-# Initialize previous detections
-prev_left_detection = None
-prev_right_detection = None
-
-# Initialize win and loss counting flags
-left_won = False
-right_won = False
-left_lost = False
-right_lost = False
+last_detection = (255, 255, 255),(255, 255, 255)
 
 while True:
     # Capture the frame
@@ -80,71 +70,76 @@ while True:
     for detection in detections:
         xmin, ymin, xmax, ymax, confidence, class_idx = detection
 
-        if xmin < (frame.shape[1] // 2):
-            left_player = detection
-        else:
-            right_player = detection
+        if confidence > 0.9:  # Apply confidence threshold
+            if xmin < (frame.shape[1] // 2):
+                left_player = detection
+            else:
+                right_player = detection
 
+    left_color = (255, 255, 255)  # White
+    right_color = (255, 255, 255)  # White
+    
     # If both players are found, referee the game
     if left_player and right_player:
+        
         # Get the objects that were detected
         left_object = classes[int(left_player[5])]
         right_object = classes[int(right_player[5])]
-
+ 
         # Determine the winner
         if (left_object == "Rock" and right_object == "Scissors") or (left_object == "Paper" and right_object == "Rock") or (left_object == "Scissors" and right_object == "Paper"):
-            if not left_won and not right_lost:
-                left_wins += 1
-                left_won = True
-                right_lost = True
             left_color = (0, 255, 0)  # Green
             right_color = (0, 0, 255)  # Red
+            if (right_color == (0, 0, 255) and left_color == (0, 255, 0)) and (last_detection == ((255, 255, 255),(255, 255, 255))):
+                left_wins += 1
+                right_losses += 1
+                last_detection = (left_color, right_color)
+            
+            
         elif (right_object == "Rock" and left_object == "Scissors") or (right_object == "Paper" and left_object == "Rock") or (right_object == "Scissors" and left_object == "Paper"):
-            if not right_won and not left_lost:
-                right_wins += 1
-                right_won = True
-                left_lost = True
             left_color = (0, 0, 255)  # Red
             right_color = (0, 255, 0)  # Green
+            if (left_color == (0, 0, 255) and right_color == (0, 255, 0)) and (last_detection == ((255, 255, 255),(255, 255, 255))):
+                right_wins += 1
+                left_losses += 1
+                last_detection = (left_color, right_color)
+            
+            
         else:
             left_color = (255, 255, 255)  # White
             right_color = (255, 255, 255)  # White
+            last_detection = (left_color, right_color)
 
-
-        if left_player != prev_left_detection or right_player != prev_right_detection:
-
-
-            # Display the results
-            cv2.putText(frame, "Left: {} | Right: {}".format(left_wins, right_wins), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
-            cv2.putText(frame, "Left Losses: {} | Right Losses: {}".format(left_losses, right_losses), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
-
-        # Update previous detections
-        prev_left_detection = left_player
-        prev_right_detection = right_player
-
-        
 
         # Draw bounding boxes for detections
         xmin, ymin, xmax, ymax, _, class_idx = left_player
         cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), left_color, 2)
-        cv2.putText(frame, classes[int(class_idx)], (int(xmin), int(ymin) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, left_color, 2)
+        cv2.putText(frame, classes[int(class_idx)], (int(xmax), int(ymax) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, left_color, 2)
         xmin, ymin, xmax, ymax, _, class_idx = right_player
         cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), right_color, 2)
-        cv2.putText(frame, classes[int(class_idx)], (int(xmin), int(ymin) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, right_color, 2)
+        cv2.putText(frame, classes[int(class_idx)], (int(xmax), int(ymax) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, right_color, 2)
 
         frame_count += 1
+        
+        left_color = None
+        right_color = None
 
-        # Get the current time
-        end_time = time.time()
+    # Display the results
+    cv2.putText(frame, "Left: {} | Right: {}".format(left_wins, right_wins), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+    cv2.putText(frame, "Left Losses: {} | Right Losses: {}".format(left_losses, right_losses), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+    cv2.putText(frame, "Model: Yolov8x", (10, frame_height - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 128, 128))
 
-        # Calculate the FPS
-        fps = frame_count / (end_time - start_time)
+    # Get the current time
+    end_time = time.time()
 
-        # Display the FPS
-        cv2.putText(frame, "FPS: {}".format(fps), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0))
+    # Calculate the FPS
+    fps = int(frame_count / (end_time - start_time))
 
-        # Write the frame to the output video
-        out_video.write(frame)
+    # Display the FPS
+    cv2.putText(frame, "FPS: {}".format(fps), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0))
+
+    # Write the frame to the output video
+    out_video.write(frame)
 
 
 

dfb343c819e5399fb175dc029efd37e97a6f909f
Ali Jawad <32601057+alijawad07@users.noreply.github.com>
2023-05-29 07:50:45 +0500

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -76,8 +76,8 @@ while True:
             else:
                 right_player = detection
 
-    left_color = (255, 255, 255)  # White
-    right_color = (255, 255, 255)  # White
+    left_color = (128, 128, 128)  # Gray
+    right_color = (128, 128, 128)  # Gray
     
     # If both players are found, referee the game
     if left_player and right_player:
@@ -90,7 +90,7 @@ while True:
         if (left_object == "Rock" and right_object == "Scissors") or (left_object == "Paper" and right_object == "Rock") or (left_object == "Scissors" and right_object == "Paper"):
             left_color = (0, 255, 0)  # Green
             right_color = (0, 0, 255)  # Red
-            if (right_color == (0, 0, 255) and left_color == (0, 255, 0)) and (last_detection == ((255, 255, 255),(255, 255, 255))):
+            if (right_color == (0, 0, 255) and left_color == (0, 255, 0)) and (last_detection == ((128, 128, 128),(128, 128, 128))):
                 left_wins += 1
                 right_losses += 1
                 last_detection = (left_color, right_color)
@@ -99,25 +99,25 @@ while True:
         elif (right_object == "Rock" and left_object == "Scissors") or (right_object == "Paper" and left_object == "Rock") or (right_object == "Scissors" and left_object == "Paper"):
             left_color = (0, 0, 255)  # Red
             right_color = (0, 255, 0)  # Green
-            if (left_color == (0, 0, 255) and right_color == (0, 255, 0)) and (last_detection == ((255, 255, 255),(255, 255, 255))):
+            if (left_color == (0, 0, 255) and right_color == (0, 255, 0)) and (last_detection == ((128, 128, 128),(128, 128, 128))):
                 right_wins += 1
                 left_losses += 1
                 last_detection = (left_color, right_color)
             
             
         else:
-            left_color = (255, 255, 255)  # White
-            right_color = (255, 255, 255)  # White
+            left_color = (128, 128, 128)  # Gray
+            right_color = (128, 128, 128)  # Gray
             last_detection = (left_color, right_color)
 
 
         # Draw bounding boxes for detections
         xmin, ymin, xmax, ymax, _, class_idx = left_player
         cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), left_color, 2)
-        cv2.putText(frame, classes[int(class_idx)], (int(xmax), int(ymax) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, left_color, 2)
+        cv2.putText(frame, classes[int(class_idx)], (int(xmin), int(ymin) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, left_color, 2)
         xmin, ymin, xmax, ymax, _, class_idx = right_player
         cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), right_color, 2)
-        cv2.putText(frame, classes[int(class_idx)], (int(xmax), int(ymax) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, right_color, 2)
+        cv2.putText(frame, classes[int(class_idx)], (int(xmin), int(ymin) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, right_color, 2)
 
         frame_count += 1
         
@@ -125,9 +125,9 @@ while True:
         right_color = None
 
     # Display the results
-    cv2.putText(frame, "Left: {} | Right: {}".format(left_wins, right_wins), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
-    cv2.putText(frame, "Left Losses: {} | Right Losses: {}".format(left_losses, right_losses), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
-    cv2.putText(frame, "Model: Yolov8x", (10, frame_height - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 128, 128))
+    cv2.putText(frame, "Left Score: {} | Right Score: {}".format(left_wins, right_wins), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+    #cv2.putText(frame, "Left Lost: {} | Right Lost: {}".format(left_losses, right_losses), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+    cv2.putText(frame, "Model: Yolov8x", (10, frame_height - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 138, 139),3)
 
     # Get the current time
     end_time = time.time()
@@ -136,7 +136,7 @@ while True:
     fps = int(frame_count / (end_time - start_time))
 
     # Display the FPS
-    cv2.putText(frame, "FPS: {}".format(fps), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0))
+    cv2.putText(frame, "FPS: {}".format(fps), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0))
 
     # Write the frame to the output video
     out_video.write(frame)
